11bfdf4b8a20bb9e38588f4aa0dec284e520a4f7
Miguel <miguel.solorio07@gmail.com>
2016-09-16 11:50:04 -0700

    Update pipeline and cleanup files

--- README.md
+++ README.md
@@ -11,31 +11,35 @@ This boilerplate runs on [Gulp](http://gulpjs.com/) and [Bower](http://bower.io/
 ## Dependencies
 
 ### Installing Bower
-1. Install Bower globally using npm: <code>npm install -g bower</code>
-2. Install project dependencies with: <code>bower install</code>.
+1. Install Bower globally using npm: ```npm install -g bower```
+2. Install project dependencies with: ```bower install```.
 3. For more details, visit [Bower's website](http://bower.io/).
 
 
 ### Installing Gulp
 1. Change to the project's root directory.
-2. Install the CLI by running: <code>npm install --global gulp-cli</code>
-3. Run Gulp with: <code>gulp</code>.
+2. Install the CLI by running: ```npm install --global gulp-cli```
+3. Run Gulp with: ```gulp```.
 4. For more details, visit [Gulp's website](http://gulpjs.com/).
 
 ## File Structure
 
 ### HTML
-All pages can be found under <code>src > template > pages</code> which use the default layout template found under <code>src > template > layouts > default.njk</code>.
+All pages can be found under ```src > template > pages``` which use the default layout template found under ```src > template > layouts > default.njk```.
 
-You can also find all partials under <code>src > template > partials > *.njk</code>.
+You can also find all partials under ```src > template > partials > *.njk```.
 
 ### SASS
-All CSS dependencies are imported at <code>src > css > main.scss</code>.
+All CSS dependencies are imported at ```src > css > main.scss```.
 
 ### JavaScript
-All JS files are part of the <code>scriptFiles</code> array in the <code>gulpfile.js</code>.
+All JS files are part of the ```scriptFiles``` array in the ```gulpfile.js```.
 
-## Build for Production
+## Developemnt
 
-### Gulp Build
-When you're ready to build for production simply run: <code>gulp build --production</code> and your files will be minified under the <code>public</code> directory.
+When working locally, running the local build command ```gulp``` will start a local server of the site and will automatically open your browser. Whenever changes are saved, the site will automaticaly regenerate your files and update your browser.
+
+*Note: If you're using Visual Studio Code, running ``` cmd+shift+b ``` will run the command in your editor without having to open the terminal. When you are done and want to stop the task, run ```cmd+shift+p``` and type ```task terminate``` to stop the task.*
+
+## Production
+Running ```gulp production``` will build and compress your pages for production.
--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 var gulp = require('gulp');
     gutil = require('gulp-util'),
     gulpif = require('gulp-if'),
+    gulpSequence = require('gulp-sequence'),
     uglify = require('gulp-uglify'),
     del = require('del'),
     cache = require('gulp-cache'),
@@ -9,6 +10,7 @@ var gulp = require('gulp');
     sass = require('gulp-sass'),
     imagemin = require('gulp-imagemin'),
     concat = require('gulp-concat'),
+    fs = require('fs'),
     browserSync = require('browser-sync').create(),
     nunjucksRender = require('gulp-nunjucks-render');
 
@@ -39,12 +41,10 @@ var scriptFiles = [
 ]
 
 // Defaults
-var isDev = true;
 var isProd = false;
 
 // Cmd line env
 if(gutil.env.prod || gutil.env.production) {
-  isDev  = false;
   isProd = true;
 }
 
@@ -92,35 +92,48 @@ gulp.task('browserSync', function() {
 });
 
 gulp.task('copy', function() {
-    return gulp.src(srcAssets.images + '**/*.+(png|jpg|gif|svg)')
+    return gulp.src(srcAssets.images + '**/*.+(png|jpg|gif)')
+      .pipe(gulp.dest(destAssets.images))
+});
+
+gulp.task('svg', function() {
+    return gulp.src(srcAssets.images + '**/*.svg')
       .pipe(gulp.dest(destAssets.images))
 });
 
 // Template
 gulp.task('template', function() {
   return gulp.src(srcAssets.root + 'template/pages/**/*.+(html|nunjucks|njk)')
-  .pipe(data(function() {
-    return require('./data.json')
-  }))
+  .pipe(data(function(file) {
+    return JSON.parse(fs.readFileSync('./navigation.json'));
+   }))
   .pipe(nunjucksRender({
     path: [srcAssets.root + 'template/']
   }))
   .pipe(gulp.dest(destAssets.root))
 });
 
+gulp.task('setProd', function() {
+  console.log('############ =========> Building files for Production');
+  isProd = true;
+});
+
 // Watch
-gulp.task('watch', ['browserSync', 'sass', 'template'], function() {
-  gulp.watch(srcAssets.styles + '**/*.scss', ['sass']);
+gulp.task('watch', ['browserSync', 'sass', 'template', 'copy'], function() {
+  gulp.watch(srcAssets.images + '**/*.+(png|jpg|gif|svg)', ['copy', 'svg']);
+  gulp.watch(srcAssets.styles + '**/*.+(scss|sass|css)', ['sass']);
   gulp.watch(srcAssets.scripts + '**/*.js', ['scripts',browserSync.reload]);
   gulp.watch(srcAssets.root + '**/*.+(html|nunjucks|njk)', ['template', browserSync.reload]);
+  gulp.watch('data.json', ['template', browserSync.reload]);
 });
 
-// Build
-gulp.task('build', ['clean','clear cache', 'template','sass','images','scripts'], function() {
-    console.log('###### => Building files');
+// Default
+gulp.task('default', ['clean','clear cache','template','sass','images','svg','scripts', 'watch'], function() {
+  console.log('############ =========> Development');
 });
 
-// Dev
-gulp.task('default', ['build', 'watch'], function() {
-  console.log('###### => Development');
-});
+// Development
+gulp.task('build', ['default']);
+
+// Production
+gulp.task('production', gulpSequence(['setProd'], 'clean','clear cache', 'template','sass','images','scripts'));
